c976fa81993897c2e4d4a9d97a07d2d446aaf70c
Gindadwi <gindadwi144@gmail.com>
2025-03-06 04:12:51 -0800

    new config new

--- config/config.js
+++ config/config.js
@@ -2,11 +2,18 @@ require("dotenv").config();
 
 module.exports = {
   development: {
-    username: DB_USERNAME,
-    password: DB_PASSWORD,
-    database: DB_NAME,
-    host: DB_HOSTNAME,
+    username: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    host: process.env.MYSQLHOST,
+    port: process.env.MYSQLPORT ? Number(process.env.MYSQLPORT) : 3306,
     dialect: "mysql",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
   production: {
     username: process.env.MYSQLUSER,
