4f98e262b160e84b6f5fd9598a627d38fd434a0f
Gindadwi <gindadwi144@gmail.com>
2025-03-06 04:04:07 -0800

    new config

--- config/config.js
+++ config/config.js
@@ -18,10 +18,17 @@ module.exports = {
     dialect: "mysql",
   },
   production: {
-    username: DB_USERNAME,
-    password: DB_PASSWORD,
-    database: DB_NAME,
-    host: DB_HOSTNAME,
+    username: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    host: process.env.MYSQLHOST,
+    port: process.env.MYSQLPORT ? Number(process.env.MYSQLPORT) : 3306,
     dialect: "mysql",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
 };
